453e2f1a2ded564135ec27012cee9cd940c5c8be
persn <perolavflaten@live.no>
2014-03-05 15:58:48 +0100

    Per Olav Flaten Backlog ID 1.2  - Added markers to GoogleMaps, this means that both the big map and the    mini map now has markers.  - Fixed bugs and flaws regarding JSON-object handling.  - Made threads and garbage collection to avoid OS-spefic troubles on    marker refreshing.

---
+++ Lommeradaren/src/no/kystverket/lommeradaren/markers/DataSourceHandler.java
@@ -0,0 +1,133 @@
+package no.kystverket.lommeradaren.markers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+/**
+ * 
+ * @author Per Olav Flaten
+ * 
+ */
+public class DataSourceHandler {
+
+	private String rawData;
+
+	private DataSource dataSource;
+	private List<POI> pointOfInterests;
+	private TextHandler textHandler;
+
+	private String lat;
+	private String lng;
+	private String alt;
+	private String radius;
+
+	public DataSourceHandler(DataSource newDataSource, String newLat,
+			String newLng, String newAlt, String newRadius) {
+		this.textHandler = new TextHandler();
+		this.dataSource = newDataSource;
+
+		this.refreshData(newLat, newLng, newAlt, newRadius);
+	}
+
+	public String getDataSourceName() {
+		return dataSource.getName();
+	}
+
+	public POI getPOI(int index) {
+		return pointOfInterests.get(index);
+	}
+
+	public int getPointOfInterestsSize() {
+		if (this.pointOfInterests != null) {
+			return this.pointOfInterests.size();
+		}
+		return 0;
+	}
+
+	/**
+	 * Refreshes POIs from the given datasource stored in the object of this
+	 * class.
+	 * 
+	 * @param newLat
+	 *            Latitude.
+	 * @param newLng
+	 *            Longitude.
+	 * @param newAlt
+	 *            Altitude.
+	 * @param newRadius
+	 *            Given radius to search for ships from current location.
+	 */
+	public void refreshData(String newLat, String newLng, String newAlt,
+			String newRadius) {
+		this.lat = newLat;
+		this.lng = newLng;
+		this.alt = newAlt;
+		this.radius = newRadius;
+
+		Thread thread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				rawData = textHandler.getRawDataFromUrl(textHandler.makeUrl(
+						dataSource.getUrl(), lat, lng, alt, radius));
+				extractAllPOI(rawData);
+			}
+		});
+		thread.start();
+	}
+
+	/**
+	 * Extracts all JSON-objects from a JSON array stored in a URL.
+	 * @param rawData
+	 */
+	private void extractAllPOI(String rawData) {
+		this.pointOfInterests = new ArrayList<POI>();
+		try {
+			JSONObject json = new JSONObject(rawData);
+			JSONArray jsonArray = json.getJSONArray("results");
+			for (int i = 0; i < jsonArray.length(); i++) {
+				this.pointOfInterests.add(extractPOIFromDataSource(jsonArray
+						.getJSONObject(i)));
+			}
+		} catch (JSONException jsonE) {
+			Log.d("no.kystverket.lommeradaren",
+					"DataSourceReader.java extractAllPOI");
+		}
+	}
+
+	/**
+	 * Extracts all information from a JSON-object.
+	 * @param currentJSON
+	 * @return
+	 */
+	private POI extractPOIFromDataSource(JSONObject currentJSON) {
+		POI pointOfInterest = null;
+		try {
+			int id = currentJSON.getInt("id");
+			double lat = currentJSON.getDouble("lat");
+			double lng = currentJSON.getDouble("lng");
+			double alt = currentJSON.getDouble("elevation");
+			String title = currentJSON.getString("title");
+			double distance = currentJSON.getDouble("distance");
+			String hasPage = currentJSON.getString("has_detail_page");
+			String webpage = currentJSON.getString("webpage");
+			String mmsi = currentJSON.getString("mmsi");
+			// double speed = currentJSON.getDouble("speed");
+			// double course = currentJSON.getDouble("course");
+			String positionTime = currentJSON.getString("positionTime");
+			pointOfInterest = new POI(id, title, lat, lng, alt, mmsi, distance,
+					hasPage, webpage, positionTime);
+		} catch (JSONException jsonE) {
+			Log.d("no.kystverket.lommeradaren",
+					"DataSourcereader.java extractPOIFromDataSource");
+		}
+		return pointOfInterest;
+	}
+
+}
--- Lommeradaren/src/no/kystverket/lommeradaren/photo/Ship.java
+++ Lommeradaren/src/no/kystverket/lommeradaren/photo/Ship.java
@@ -2,6 +2,11 @@ package no.kystverket.lommeradaren.photo;
 
 import no.kystverket.lommeradaren.markers.POI;
 
+/**
+ * 
+ * @author Henrik Reitan
+ *
+ */
 public class Ship extends POI {
 
 	private double speed;
@@ -11,11 +16,11 @@ public class Ship extends POI {
 
 
 	public Ship(int id, String name, double lat, double lng, double alt,
-			String mmsi, double distance, boolean has_detail_page, String webpage,
+			String mmsi, double distance, String has_detail_page, String webpage,
 			String positionTime, String imo, double speed, String lastPort,
 			String destination, double course) {
 		super(id, name, lat, lng, alt, mmsi, distance, has_detail_page,
-				webpage, positionTime, imo);
+				webpage, positionTime);
 		this.speed = speed;
 		this.lastPort = lastPort;
 		this.destination = destination;
