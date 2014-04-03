080cfc40b4dfdc29f2239b92192b6a029b296e1a
Umethar <henrik.reitan@gmail.com>
2014-04-03 10:07:01 +0200

    Henrik Reitan #1.8 Implemented interpolation of the values for orientation (bearing) in sensorHandler

--- Lommeradaren/src/no/kystverket/lommeradaren/camera/augmented/SensorHandler.java
+++ Lommeradaren/src/no/kystverket/lommeradaren/camera/augmented/SensorHandler.java
@@ -1,5 +1,7 @@
 package no.kystverket.lommeradaren.camera.augmented;
 
+import java.util.LinkedList;
+
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
@@ -15,7 +17,10 @@ public class SensorHandler {
 	private float[] gravity = { 0, 0, 0 };
 	private float[] magnetic = { 0, 0, 0 };
 	private final float alpha = 0.15f;
-
+	private LinkedList<float[]> values = new LinkedList<float[]>();
+	private int counter = 0;
+	
+	
 	public boolean handleEvent(SensorEvent evt) {
 		if (evt.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
 			lowPass(evt.values.clone(), gravity);
@@ -28,14 +33,35 @@ public class SensorHandler {
 			float inclinationMatrix[] = new float[9];
 			if (SensorManager.getRotationMatrix(rotationMatrix,
 					inclinationMatrix, gravity, magnetic)) {
-				orientation = new float[3];
-				orientation = SensorManager.getOrientation(rotationMatrix,
-						orientation);
+				float orientationMatrix[] = new float[3];
+				orientationMatrix = SensorManager.getOrientation(rotationMatrix,
+						orientationMatrix);
+				orientation = interpolate(orientationMatrix);
 				return true;
 			} else {
 				return false;
 			}
 		}
+	}
+
+	public float[] interpolate(float[] newValues){
+		if(values.size() < 20 ){
+			values.add(newValues);
+		} else {
+			values.set(counter, newValues);
+			counter++;
+			counter = counter%20;
+		}
+		float[] res = new float[3];
+		for (float[] f : values){
+			res[0] += f[0];
+			res[1] += f[1];
+			res[2] += f[2];
+		}
+		res[0] = res[0] / values.size();
+		res[1] = res[1] / values.size();
+		res[2] = res[2] / values.size();
+		return res;
 	}
 
 	public float[] getOrientation() {
